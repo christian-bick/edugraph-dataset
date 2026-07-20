c3855dfb36fb7333aefb362761bc4892e6795173
Christian Bick <christian.bick@bitsuppliers.com>
2026-07-20 19:15:51 +0200

    Fix geometry-build-shape

--- src/generators/geometry-build-shape/spec.ts
+++ src/generators/geometry-build-shape/spec.ts
@@ -1,13 +1,21 @@
 import {GeneratorSpec} from '../../types/generator-spec.ts';
-import {Area} from 'edugraph-ts';
+import {Area, Scope} from 'edugraph-ts';
+import {ConfigFromSchema} from "../../types/schema.ts";
 
 export const spec: GeneratorSpec = {
     generatorId: 'geometry-build-shape',
     generalLabels: [
+        Area.ShapeRecognition,
+        Scope.ShapeProperties
+    ],
+};
+
+export const GeometryBuildShapeGeneratorSchema = {
+    target: [
         Area.Triangle,
         Area.Square,
-        Area.Hexagon,
-        Area.Rectangle,
-        Area.ShapePlotting
+        Area.Rectangle
     ]
-};
+} as const;
+
+export type GeometryBuildShapeGeneratorConfig = ConfigFromSchema<typeof GeometryBuildShapeGeneratorSchema>;
