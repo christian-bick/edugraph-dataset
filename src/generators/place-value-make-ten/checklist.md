8ebf471604196e4382e80d13bc6b5b9c6a039efd
Christian Bick <christian.bick@bitsuppliers.com>
2026-07-20 22:51:02 +0200

    Fix place-value-make-ten

---
+++ src/generators/place-value-make-ten/checklist.md
@@ -0,0 +1,7 @@
+### Place Value Make Ten Visual Checklist
+
+- The problem displays two Ten Frames representing a decomposition of 10.
+- The first frame shows the "givenNumber" of dots in rose/red color.
+- The second frame shows the "missingNumber" of dots in amber/yellow color.
+- The equation displays: "givenNumber + [ ] = 10".
+- In solution mode, the correct "missingNumber" is displayed inside the equation box in green color.
---
+++ src/spec/test/place-value-make-ten.ts
@@ -0,0 +1,19 @@
+import DatasetPermutationBuilder from '../../lib/dataset-permutation-builder.ts';
+import { Area, Scope, Ability } from 'edugraph-ts';
+import { CompetencyTarget } from '../ccss/kindergarten.ts';
+
+const builder = new DatasetPermutationBuilder()
+    .addLabels([
+        Area.Difference,
+        Ability.ProcedureExecution
+    ])
+    .applyLabelVariants([
+        [Scope.NumbersWithZero],
+        [Scope.NumbersWithoutZero]
+    ]);
+
+export const PlaceValueMakeTenTestSpec: CompetencyTarget[] = builder.build().map((p, i) => ({
+    id: `test-place-value-make-ten-${i}`,
+    labels: p.labels,
+    constraints: p.constraints
+}));
